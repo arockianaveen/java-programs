67023c3e698c6a86491502d56a67f1b31edf2d9a
arockianaveen <116068547+arockianaveen@users.noreply.github.com>
2022-10-18 15:39:50 +0530

    Add files via upload

---
+++ DateTypeExample.java
@@ -0,0 +1,36 @@
+package datatype;
+
+public class DateTypeExample {
+
+	public static void main(String[] args) {
+		// //declaration of variables
+		int intval;
+		float fval;
+		double dval;
+		char cval;
+		short sval;
+		byte bval;
+		long lval;
+		boolean boolval;
+		
+		//Assign the values
+		intval=468;
+		
+		System.out.println("Integer value=" +intval);
+		fval=468.5f;
+		System.out.println("Float value=" +fval);
+		
+		dval=468.9;
+		cval='A';
+		lval=9871;
+		boolval=true;
+		
+		System.out.println("Double value=" +dval);
+		System.out.println("Char value=" +cval);
+		System.out.println("Long value=" +lval);
+		System.out.println("Boolean value=" +boolval);
+
+
+	}
+
+}
