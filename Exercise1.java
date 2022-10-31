0f4d5b43941cc8a8d966f5a929b50864f83ef3ab
arockianaveen <116068547+arockianaveen@users.noreply.github.com>
2022-10-31 12:52:17 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,43 @@
+package exercise;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//boolean
+				boolean IoT = true;
+				System.out.println(IoT);
+
+				//byte
+				byte range;
+				range = 104;
+				System.out.println(range);
+
+				//short
+				short range1;
+				range1 = -196;
+				System.out.println(range1);
+
+				//int
+				int range2 = 500000000;
+				System.out.println(range2);
+
+				//long
+				long range3 = 60000000000L;
+				System.out.println(range3);
+
+				//double
+				double number = 88.7;
+				System.out.println(number);
+
+				//float
+				float number1 = 86.4f;
+				System.out.println(number1);
+
+				//char (Unicode value of A is '\u0041')
+				char letter = '\u0041';
+				System.out.println(letter);
+	
+
+}
+}
