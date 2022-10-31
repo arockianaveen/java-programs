e30784c6880c44eb094a72edb16af699beb5d228
arockianaveen <116068547+arockianaveen@users.noreply.github.com>
2022-10-31 10:59:43 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,17 @@
+package area;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		double pi,radius,area;
+		pi = 3.1415;
+		radius = 5;
+		area = pi*radius*radius;
+		System.out.println("Area of Circle is ="+area);
+
+
+	}
+
+}
