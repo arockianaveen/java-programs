6ae0e03aa490ef3d82ebb5741c19073907c6a4bf
arockianaveen <116068547+arockianaveen@users.noreply.github.com>
2022-10-31 11:00:53 +0530

    Add files via upload

---
+++ AreaOfSquare.java
@@ -0,0 +1,12 @@
+package area;
+
+public class AreaOfSquare {
+
+	public static void main(String[] args) {
+		float a,area;
+		a=12.3f;
+		area=a*a;
+		System.out.println("Area of square is="+area);
+	}
+
+}
