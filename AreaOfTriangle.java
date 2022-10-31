a6cbc05eb06b9a933770cf19a6915636c55426a6
arockianaveen <116068547+arockianaveen@users.noreply.github.com>
2022-10-31 11:00:23 +0530

    Add files via upload

---
+++ AreaOfTriangle.java
@@ -0,0 +1,15 @@
+package area;
+
+public class AreaOfTriangle {
+
+	public static void main(String[] args) {
+		int breadth = 4;
+		float area;
+		float height = 4.5f;
+		area=breadth*height/2;
+		System.out.println("Area of triangle is="+area);
+		
+
+	}
+
+}
